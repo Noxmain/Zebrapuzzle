e3b081eb1adf9f7afc8d07e389d0e06e19cd7750
Noxmain <noah.brandt@me.com>
2021-06-22 21:43:00 +0200

    Added generate function

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ function shuffle(a) {
     return a;
 }
 
+Array.prototype.insert = function(index, item) {
+  this.splice(index, 0, item);
+};
+
 function Category(c, g) {
   this.content = c;
   this.grammar = g; // Die Person, die ..., wohnt neben der Person, die ....
@@ -31,34 +35,76 @@ function Category(c, g) {
     }
     return new Category(limited, this.grammar);
   };
-  this.grammatical = function(i) {
-    return this.grammar.replace("%", this.content[i]);
+  this.grammatical = function(i, m) {
+    return this.grammar[m].replace("%", this.content[i]);
   };
 }
 
 const categories = [
-  new Category(["rot", "violett", "blau", "grün", "gelb", "braun", "türkis", "weiß", "grau", "schwarz"], "im %en Haus wohnt"),
-  new Category(["Peter", "Marie", "Paul", "Anna", "Tim", "Emma", "Linus", "Mia", "Till", "Luisa"], "% heißt"),
-  new Category(["Pizza", "Pasta", "Kartoffeln", "Reis", "Curry", "Obst", "Gemüse", "Burger", "Brot", "Kuchen"], "% isst"),
-  new Category(["Mineralwasser", "Apfelsaft", "Orangensaft", "Milch", "Tee", "Limonade", "Kakao", "Bier", "Wein", "Sprudelwasser"], "% trinkt"),
-  new Category(["Hunde", "Katzen", "Hamster", "Meerschweinchen", "Wellensittiche", "Fische", "Mäuse", "Krebse", "Kanninchen", "Schildkröten"], "% hat"),
-  new Category(["Bäckerei", "Tischlerei", "Druckerei", "Schneiderei", "Fleischerei", "Konditorei", "Fahrradwerkstatt", "Autowerkstatt", "Bibliothek", "Goldschmiede"], "in einer % arbeitet"),
+  new Category(["rot", "violett", "blau", "grün", "gelb", "braun", "türkis", "weiß", "grau", "schwarz"], ["im %en Haus wohnt", "wohnt im %en Haus"]),
+  new Category(["Peter", "Marie", "Paul", "Anna", "Tim", "Emma", "Linus", "Mia", "Till", "Luisa"], ["% heißt", "heißt %"]),
+  new Category(["Pizza", "Pasta", "Kartoffeln", "Reis", "Curry", "Obst", "Gemüse", "Burger", "Brot", "Kuchen"], ["% isst", "isst %"]),
+  new Category(["Mineralwasser", "Apfelsaft", "Orangensaft", "Milch", "Tee", "Limonade", "Kakao", "Bier", "Wein", "Sprudelwasser"], ["% trinkt", "trinkt %"]),
+  new Category(["Hunde", "Katzen", "Hamster", "Meerschweinchen", "Wellensittiche", "Fische", "Mäuse", "Krebse", "Kanninchen", "Schildkröten"], ["% hat", "hat %"]),
+  new Category(["Bäckerei", "Tischlerei", "Druckerei", "Schneiderei", "Fleischerei", "Konditorei", "Fahrradwerkstatt", "Autowerkstatt", "Bibliothek", "Goldschmiede"], ["in einer % arbeitet", "arbeitet in einer %"]),
 ];
 
 function Puzzle() {
   this.solution = [];
   this.conditions = [];
-  this.categories = [];
-
-  this.conditions_string = function() {
-
-  };
-  this.solution_string = function() {
-
-  };
+  this.question = "";
 
   this.generate = function() {
+    let c = [];
+    while (c.length < 5) {
+      let r = Math.floor(Math.random() * categories.length);
+      if (!isin(categories[r], c)) {c.push(categories[r]);}
+    }
+    c = c.map(function(x) {return x.limit(5);});
 
+    this.solution = [];
+    for (let i = 0; i < 5; i++) {
+      this.solution.push(clone(c[i].content));
+    }
+
+    this.conditions = [];
+    let o = shuffle([0, 2, 3]);
+    o.insert(o.indexOf(0) + 1, 1);
+    o.insert(o.indexOf(3) + 1, 4);
+    let context = function(a1, a2, r, b1, b2) {
+      if (r == "=") {return "Die Person, die " + c[o[a1]].grammatical(a2, 0) + ", wohnt im " + (o[b1] + 1) + ". Haus.";}
+      else if (r == "s") {return "Die Person, die " + c[o[a1]].grammatical(a2, 0) + ", " + c[o[b1]].grammatical(b2, 1) + ".";}
+      else if (r == "r") {return "Die Person, die " + c[o[a1]].grammatical(a2, 0) + ", wohnt rechts neben der Person, die " + c[o[b1]].grammatical(b2, 0) + ".";}
+      else if (r == "l") {return "Die Person, die " + c[o[a1]].grammatical(a2, 0) + ", wohnt links neben der Person, die " + c[o[b1]].grammatical(b2, 0) + ".";}
+      else if (r == "n") {return "Die Person, die " + c[o[a1]].grammatical(a2, 0) + ", wohnt neben der Person, die " + c[o[b1]].grammatical(b2, 0) + ".";}
+    };
+    let t = function(x) {
+      if (isin(x, ["A", "B", "C", "D", "E"])) {return ["A", "B", "C", "D", "E"].indexOf(x);}
+      if (isin(x, ["1", "2", "3", "4", "5"])) {return ["1", "2", "3", "4", "5"].indexOf(x);}
+    };
+
+    this.conditions.push(context(t("B"), t("1"), "s", t("A"), t("1")));
+    this.conditions.push(context(t("B"), t("2"), "s", t("E"), t("1")));
+    this.conditions.push(context(t("B"), t("3"), "s", t("C"), t("1")));
+    this.conditions.push(context(t("A"), t("2"), "l", t("A"), t("3")));
+    this.conditions.push(context(t("A"), t("2"), "s", t("C"), t("2")));
+    this.conditions.push(context(t("D"), t("2"), "s", t("E"), t("2")));
+    this.conditions.push(context(t("C"), t("3"), "=", 3));
+    this.conditions.push(context(t("A"), t("4"), "s", t("D"), t("2")));
+    this.conditions.push(context(t("D"), t("3"), "n", t("E"), t("3")));
+    this.conditions.push(context(t("D"), t("4"), "s", t("C"), t("4")));
+    this.conditions.push(context(t("B"), t("4"), "=", 1));
+    this.conditions.push(context(t("E"), t("4"), "n", t("D"), t("2")));
+    this.conditions.push(context(t("B"), t("5"), "s", t("D"), t("5")));
+    this.conditions.push(context(t("B"), t("4"), "n", t("A"), t("5")));
+    this.conditions.push(context(t("D"), t("3"), "n", t("C"), t("5")));
+    this.conditions = shuffle(this.conditions);
+    this.question = "Welche Person " + c[o[t("E")]].grammatical(t("5"), 1) + "?";
   };
   this.generate();
 }
+
+let puzzle = new Puzzle();
+console.log(puzzle.conditions.join("\n"));
+console.log(puzzle.question);
+console.log(puzzle.solution);
