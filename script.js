0548d6b84a7608b55ed6a28e5dfbd771d5040a3b
Noxmain <noah.brandt@me.com>
2021-06-22 13:36:50 +0200

    Added Puzzle class

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // By Noxmain
 
 function Puzzle() {
+  this.solution = [];
+  this.conditions = [];
+  this.categories = [];
 
+  this.conditions_string = function() {
+
+  };
+  this.solution_string = function() {
+
+  };
+
+  this.generate = function() {
+
+  };
+  this.generate();
 }
