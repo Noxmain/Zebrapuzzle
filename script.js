437397b4807afdebdb269487b7dd089b58d751a3
Noxmain <noah.brandt@me.com>
2021-06-22 13:38:12 +0200

    Added necessary functions

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 // By Noxmain
 
+/*
+relation              in code  logic
+a is b                "a=b"    a = b
+a is same as b        "asb"    a - b = 0
+a is right next to b  "arb"    a - b = 1
+a is left next to b   "alb"    a - b = -1
+a is next to b        "anb"    |a - b| = 1
+*/
+
+function clone(o) {
+  if (o === undefined || typeof o !== 'object') {return o;}
+  let tmp = Object.create(o);
+  for (let key in o) {tmp[key] = clone(o[key]);}
+  return tmp;
+}
+
+function isin(a, b) {
+  return (b.indexOf(a) >= 0);
+}
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
 function Puzzle() {
   this.solution = [];
   this.conditions = [];
